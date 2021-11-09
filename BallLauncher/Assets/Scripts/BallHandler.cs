8b3be2ea6ea75caf769c0c1e34ff2f8b4dad2052
Jean-Pierre Van Loggerenberg <jploggies@gmail.com>
2021-11-09 08:14:07 +0100

    BallHandling Mechanics
    Added world pos detection
    Added mobile input touch detection
    Added Ball Kinematics and sprintjoint

---
+++ BallLauncher/Assets/Scripts/BallHandler.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BallHandler : MonoBehaviour
+{
+
+    [SerializeField] private Rigidbody2D currentBallRigidBody;
+
+    private Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(!Touchscreen.current.primaryTouch.press.isPressed)
+      {
+        currentBallRigidBody.isKinematic = false;
+        return;
+      }
+
+      currentBallRigidBody.isKinematic = true;
+
+      Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();  
+
+      Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
+
+      currentBallRigidBody.position = worldPosition;
+ 
+    }
+}
